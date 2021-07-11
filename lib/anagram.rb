97b07b6e88f4f4e5a0cda09c444deda2d73eb32e
ppidcard <tvxqlikei@live.cn>
2021-07-12 06:33:01 +1000

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram 
+    def initialize(word)
+        @word = word
+    end
+    def match(words)
+        res = []
+        words.each do |val|
+           res << val if val.split('').sort == @word.split('').sort 
+            
+        end
+        res
+    end
+end
+
+word = Anagram.new('listen')
+word.match(%w(enlists google inlets banana))
+word.match(%w(aaaaaaaanana))
